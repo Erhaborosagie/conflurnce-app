d5d36b29172bb71c3854ab1a9d8b5d2d8fa7a023
Osagie Erhabor <erhaborosagie@gmail.com>
2022-11-10 13:29:33 +0100

    Initial Bootstrap

---
+++ src/main/java/com/osagie/confluenceapp/ConfluenceAppApplication.java
@@ -0,0 +1,13 @@
+package com.osagie.confluenceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfluenceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfluenceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osagie/confluenceapp/ConfluenceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osagie.confluenceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfluenceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
